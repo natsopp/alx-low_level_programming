70578a919988ee3294dba538f4a8ca7ea3def028
natsopp <yaaichyounes96@gmail.com>
2023-03-16 15:09:23 -0700

    C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- * Return:1
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
  */
 int main(void)
 {
-fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+  write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+  return 1;
 }
