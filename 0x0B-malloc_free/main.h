47518ed1c5a4cc960db96e069f5067681df34852
natsopp <yaaichyounes96@gmail.com>
2023-04-11 15:36:54 -0700

    malloc

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-#include <stdio.h>
-
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -13,4 +10,4 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
 
-#endif /* MAIN_H */
+#endif
