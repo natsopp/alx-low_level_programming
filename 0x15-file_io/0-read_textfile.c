e3c6a97f7342a22f72f3d36ef3efd9ffadcb14c2
yaaich <yaaichyounes96@gmail.com>
2023-05-08 16:59:16 -0700

    file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,46 @@
-#include "berry.h"
+#include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to the file name
- * @letters: the number of letters it should read and print
- * Return:  the actual number of letters it could read and print
-*/
+ * read_textfile - reads a text file and prints it to the POSIX stdout
+ * @filename: text file to read
+ * @letters: number of letters it should read and print
+ * Return: actual no of letters it could read and print
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
+
 {
+	int fd;
+	char *buffer;
+	ssize_t read_counter, write_counter = 0;
 
-int file;
-ssize_t rcount, wcount;
-char *buffer;
-
-if (filename == NULL)
-	return (0);
-
-file = open(filename, O_RDWR);
-if (file == -1)
-	return (0);
-
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
-{
+	if (filename == NULL)
+		return (0);
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	read_counter = read(fd, buffer, letters);
+	if (read_counter == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	write_counter = write(STDOUT_FILENO, buffer, read_counter);
+	if (write_counter == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	close(fd);
 	free(buffer);
-	return (0);
-}
 
-rcount = read(file, buffer, letters);
-if (rcount == -1)
-	return (0);
-
-wcount = write(STDOUT_FILENO, buffer, rcount);
-
-if (wcount == -1 || rcount != wcount)
-	return (0);
-
-free(buffer);
-
-close(file);
-
-return (wcount);
+	return (write_counter);
 }
