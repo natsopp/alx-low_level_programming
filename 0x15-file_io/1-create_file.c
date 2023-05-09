9b11a3e947d5b24c03804c07704068b4c0134215
yaaich <yaaichyounes96@gmail.com>
2023-05-08 17:01:46 -0700

    file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,37 @@
 #include "main.h"
 
 /**
- * create_file - create a file with read/write access for user
- * @filename: name of file to create
- * @text_content: string to write to file
+ * create_file - creates a file
+ * @filename: the name of the file to create
+ * @text_content: NULL terminated string to write to file
+ *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, rstatus, i;
+	int fd;
+	size_t i = 0;
+	ssize_t write_counter;
 
-if (filename == NULL)
-	return (-1);
-
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-
-if (fd == -1)
-	return (-1);
-
-if (text_content)
-{
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	rstatus = write(fd, text_content, i);
-	if (rstatus == -1)
+	if (filename == NULL)
 		return (-1);
-}
 
-close(fd);
-return (1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+		return (1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	/* finding the length of a text_content string */
+	while (text_content[i] != 0)
+		i++;
+	write_counter = write(fd, text_content, i);
+	if (write_counter == -1)
+		return (-1);
+	close(fd);
+	return (1);
 }
